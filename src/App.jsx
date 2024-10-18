4798dcd5ad55ede82f53141e2016c51cfb704f3c
Vladyslav Natalych <v.natalich05@gmail.com>
2024-10-18 16:28:56 +0300

    Add structure of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { getFilms } from './apiServices/fetchApi'
+
+import Navigation from './components/Navigation/Navigation';
+import NavLink from './components/NavLink/NavLink';
 
 function App() {
+  getFilms();
   return (
     <>
-      
+      <NavLink />
+      <Navigation/>
     </>
   )
 }
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <>
+        <Routes>
+              <Route></Route>
+              <Route></Route>
+              <Route></Route>
+              <Route></Route>
+              <Route></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default Navigation
